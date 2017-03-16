0d757a23cdcb552ffa1ef63a202be193bed89a1f
Đăng Quang <ndq.quang511@gmail.com>
2017-03-16 21:12:26 +0700

    giao diện facebook

--- GoGoTravel/app/src/main/java/vn/edu/greenacademy/gogotravel/MainActivity.java
+++ GoGoTravel/app/src/main/java/vn/edu/greenacademy/gogotravel/MainActivity.java
@@ -2,12 +2,28 @@ package vn.edu.greenacademy.gogotravel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvStatus;
+    Button btnLogInFacebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvStatus = (TextView) findViewById(R.id.tvStatus);
+        btnLogInFacebook = (Button) findViewById(R.id.btnLogInFacebook);
+
+        btnLogInFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
