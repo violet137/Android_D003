6c077fb6a06f3af12dff32902f72d5d5dff91d2e
Phạm Anh <phamanh1289@gmail.com>
2017-03-18 19:57:32 +0700

    Update SHA1

---
+++ GoGoTravel/app/src/main/java/vn/edu/greenacademy/Unitl/Constrant.java
@@ -0,0 +1,10 @@
+package vn.edu.greenacademy.Unitl;
+
+/**
+ * Created by GIT on 3/18/2017.
+ */
+
+public class Constrant {
+
+    private static final int SIGN_IN_GG = 007;
+}
--- GoGoTravel/app/src/main/java/vn/edu/greenacademy/gogotravel/MainActivity.java
+++ GoGoTravel/app/src/main/java/vn/edu/greenacademy/gogotravel/MainActivity.java
@@ -1,13 +1,50 @@
 package vn.edu.greenacademy.gogotravel;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private ProgressDialog mProgressDialog;
+
+    private SignInButton btnSignIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnSignIn = (SignInButton) findViewById(R.id.btn_sign_in);
+
+        btnSignIn.setOnClickListener(this);
+
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        btnSignIn.setSize(SignInButton.SIZE_STANDARD);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
