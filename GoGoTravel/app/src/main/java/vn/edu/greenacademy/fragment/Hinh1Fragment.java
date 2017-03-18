1bba60d6a52a3e5e24cbbe4ca42fcd028fbcd66f
DESKTOP-VV3L5PU\User <nguyenhieuwd@gmail.com>
2017-03-18 18:11:57 +0700

    code

---
+++ GoGoTravel/app/src/main/java/vn/edu/greenacademy/fragment/Hinh1Fragment.java
@@ -0,0 +1,38 @@
+package vn.edu.greenacademy.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vn.edu.greenacademy.gogotravel.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Hinh1Fragment extends Fragment {
+    private static Hinh1Fragment instance = null;
+
+    public static Hinh1Fragment getInstance(){
+        if (instance==null){
+            instance = new Hinh1Fragment();
+        }
+        return instance;
+    }
+
+
+    public Hinh1Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hinh1, container, false);
+    }
+
+}
