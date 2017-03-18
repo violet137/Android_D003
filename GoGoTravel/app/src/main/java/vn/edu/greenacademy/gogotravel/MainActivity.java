594a93c3826aa40b1f4b3bfc256e59e799e56750
Phước Minh <phuocminh6464@gmail.com>
2017-03-18 21:33:25 +0700

    màn hình đăng ký

--- GoGoTravel/app/src/main/java/vn/edu/greenacademy/gogotravel/MainActivity.java
+++ GoGoTravel/app/src/main/java/vn/edu/greenacademy/gogotravel/MainActivity.java
@@ -1,7 +1,7 @@
 package vn.edu.greenacademy.gogotravel;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        
     }
 }
