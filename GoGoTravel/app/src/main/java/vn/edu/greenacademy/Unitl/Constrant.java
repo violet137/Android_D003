25a79f93482f6613833002395b34d508bffdb73c
Phạm Anh <phamanh1289@gmail.com>
2017-03-24 00:36:32 +0700

    Validate Login..

--- GoGoTravel/app/src/main/java/vn/edu/greenacademy/Unitl/Constrant.java
+++ GoGoTravel/app/src/main/java/vn/edu/greenacademy/Unitl/Constrant.java
@@ -8,8 +8,7 @@ public class Constrant {
 
     public static final int SIGN_IN_GG = 007;
 
-    public static final String SCOPES = "https://www.googleapis.com/auth/plus.login "
-            + "https://www.googleapis.com/auth/drive.file";
     public static final int REQUEST_CODE_TOKEN_AUTH = 001;
+
 
 }
--- GoGoTravel/app/src/main/java/vn/edu/greenacademy/gogotravel/MainActivity.java
+++ GoGoTravel/app/src/main/java/vn/edu/greenacademy/gogotravel/MainActivity.java
@@ -1,10 +1,8 @@
 package vn.edu.greenacademy.gogotravel;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -17,22 +15,18 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.google.android.gms.auth.GoogleAuthException;
-import com.google.android.gms.auth.GoogleAuthUtil;
-import com.google.android.gms.auth.UserRecoverableAuthException;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 import vn.edu.greenacademy.Unitl.Constrant;
 
@@ -40,8 +34,6 @@ import vn.edu.greenacademy.Unitl.Constrant;
 public class MainActivity extends AppCompatActivity implements
         View.OnClickListener,
         GoogleApiClient.OnConnectionFailedListener {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
 
     private GoogleApiClient mGoogleApiClient;
     private ProgressDialog mProgressDialog;
@@ -52,6 +44,8 @@ public class MainActivity extends AppCompatActivity implements
     private ImageView imgProfilePic;
     private TextView txtName, txtEmail;
     public  String accessToken = "";
+    private static boolean checkID = true;
+    private ArrayList<String> arrID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +58,8 @@ public class MainActivity extends AppCompatActivity implements
         imgProfilePic = (ImageView) findViewById(R.id.imgProfilePic);
         txtName = (TextView) findViewById(R.id.txtName);
         txtEmail = (TextView) findViewById(R.id.txtEmail);
+
+        arrID = new ArrayList();
 
         btnSignIn.setOnClickListener(this);
         btnSignOut.setOnClickListener(this);
@@ -100,41 +96,52 @@ public class MainActivity extends AppCompatActivity implements
                     }
                 });
     }
-
-
-
-
-
     private void loginGoogle(GoogleSignInResult result) {
         if (result.isSuccess()) {
+
             // Login thành công
             final GoogleSignInAccount acct = result.getSignInAccount();
 
             String personName = acct.getDisplayName();
             Uri personPhotoUrl  = acct.getPhotoUrl();
             String email = acct.getEmail();
-
-            //id đăng ký account & token season
+            //id đăng ký account
             String id = acct.getId();
 
-            new RetrieveTokenTask().execute(personName);
-
-
-            txtName.setText(personName + "\n" + id + "\n" + accessToken);
-            txtEmail.setText(email);
-
-            //Nếu link hình ảnh Null thì lấy hình mặc định
-
-            if (personPhotoUrl == null){
-                imgProfilePic.setImageResource(R.drawable.common_google_signin_btn_icon_dark);
+            if (arrID.size() == 0){
+                checkID = true;
             }else {
-                Glide.with(getApplicationContext()).load(personPhotoUrl.toString())
-                        .thumbnail(0.5f)
-                        .crossFade()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(imgProfilePic);
+
+                for (int i = 0; i < arrID.size(); i++) {
+                    if (id.equals(arrID.get(i))){
+                        checkID = false;
+                        break;
+                    }else {
+                        checkID = true;
+                    }
+                }
             }
 
+            //------
+            if (checkID == false){
+                txtName.setText("Đã đăng nhập rồi");
+                txtEmail.setText("");
+                imgProfilePic.setImageResource(R.drawable.common_full_open_on_phone);
+            }else {
+                arrID.add(id);
+                txtName.setText(personName);
+                txtEmail.setText(email);
+                //Nếu link hình ảnh Null thì lấy hình mặc định
+                if (personPhotoUrl == null){
+                    imgProfilePic.setImageResource(R.drawable.common_google_signin_btn_icon_dark);
+                }else {
+                    Glide.with(getApplicationContext()).load(personPhotoUrl.toString())
+                            .thumbnail(0.5f)
+                            .crossFade()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(imgProfilePic);
+                }
+            }
             updateUI(true);
         } else {
             // Signed out
@@ -196,38 +203,6 @@ public class MainActivity extends AppCompatActivity implements
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Toast.makeText(MainActivity.this,"Login Faild", Toast.LENGTH_LONG).show();
     }
-
-    private class RetrieveTokenTask extends AsyncTask<String, Void, String> {
-
-
-        @Override
-        protected String doInBackground(String... strings) {
-
-            String accountName = strings[0];
-            String scope = "oauth2:" + Scopes.PROFILE + " " + "email";
-            Context context = getApplicationContext();
-            String accessToken = "";
-
-            try {
-                accessToken = GoogleAuthUtil.getToken (context, accountName, scope);
-            } catch (IOException e) {
-                 e.getMessage();
-            } catch (UserRecoverableAuthException e) {
-//                startActivityForResult (e.getIntent(), Constrant.REQUEST_CODE_TOKEN_AUTH);
-            } catch (GoogleAuthException e) {
-                e.getMessage();
-
-            }
-            return accessToken;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            accessToken = s;
-        }
-    };
-
 
     private void showProgressDialog() {
         if (mProgressDialog == null) {
